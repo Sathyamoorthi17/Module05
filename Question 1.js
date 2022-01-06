1a7453839a940454641c34fa97219dbe65191004
Sathyamoorthi17 <85884448+Sathyamoorthi17@users.noreply.github.com>
2022-01-06 18:49:40 +0530

    Update Question 1.js

--- Question 1.js
+++ Question 1.js
@@ -1,1 +1,97 @@
 // programs in anonymous function & IIFE
+
+// 1. Print odd numbers in an array
+
+var sum = function(){
+    for(let i=0; i<10; i++) {
+        if(i % 2 == 1){
+            console.log(i);
+        }
+    
+    }
+         return i ;
+};
+
+console.log(sum());
+
+//Output :  1 3 5 7 9
+
+
+// 2.Convert all the strings to title caps in a string array
+
+var tittlecaps = function(str){
+    
+    str = str.toLowerCase();
+    
+    str = str.split(' ');
+    
+    for(var i=0; i<str.length; i++){
+        
+        str[i] = str[i].toUpperCase()
+        
+    }
+    
+    return str.join(' ');
+}
+
+console.log(tittlecaps("I'm a WebdEveloper"));
+
+//Output : I'M A WEBDEVELOPER
+
+
+//3.Sum of all numbers in an array
+
+const sum = [17,27,3,58,94];
+
+let totalsum = sum.reduce(function(previousVal,currentVal){
+    return previousVal + currentVal ;
+    
+    
+});
+
+console.log(totalsum);
+
+//Output : 199
+
+
+//4. Return all the prime numbers in an array
+
+var isPrime = function(num) {
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+};
+
+var display = function(n) {
+    var arr = [2];
+    for ( var i = 3; i < n; i+=2 ) {
+        if ( isPrime(i) ) {
+            arr.push(i);
+        }
+    }
+    return arr;
+};
+
+console.log(display(20));
+
+//Output : [ 2, 3, 5, 7, 11, 13, 17, 19 ]
+
+
+//5.Return all the palindromes in an array
+
+var arr = ["civic","books","read", "radar", "level"];
+
+var b = arr.filter(function(c,d,f){
+    
+    var Cur = c.split('').reverse().join('');
+
+        if(c == Cur){
+        console.log( arr[d] );
+}
+
+});
+
+//Output : civic radar level
